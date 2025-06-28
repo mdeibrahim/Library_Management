5cc50d15ccdffb3802a69e53bd4e7a17ed42e7fc
Md Eibrahim <mmeibrahim505@gmail.com>
2025-06-28 06:43:09 +0600

    first commit

---
+++ apps/member/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Member, Book, Profile, Author, BorrowRecord
+
+@admin.register(Member)
+class MemberAdmin(ModelAdmin):
+    list_display = ('id', 'email', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+
+@admin.register(Profile)
+class ProfileAdmin(ModelAdmin):
+    list_display = ('id', 'member', 'name', 'age', 'gender', 'address', 'phone')
+
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    list_display = ('id', 'member', 'title', 'author', 'published_date', 'category', 'available')
+    list_filter = ('available', 'category')
+
+@admin.register(Author)
+class AuthorAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'bio')
+
+@admin.register(BorrowRecord)
+class BorrowRecordAdmin(ModelAdmin):
+    list_display = ('id', 'member', 'book', 'borrow_date', 'return_date')
+    list_filter = ('borrow_date', 'return_date')
---
+++ apps/member/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Member, Profile
+
+@receiver(post_save, sender=Member)
+def create_member_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(member=instance)
+        # Profile.objects.create(
+        #     member=instance,
+        #     name=f"{instance.first_name} {instance.last_name}".strip(),
+        #     age=0,  # default age
+        #     gender='other',  # default gender
+        #     address='',
+        #     phone=''
+        # )
